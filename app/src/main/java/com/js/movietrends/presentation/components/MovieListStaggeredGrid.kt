b59fdd4ed786bd30990295109ab017cc8dc77c3e
Jisu Kim <kjisubit@gmail.com>
2025-01-19 20:26:30 +0900

    refactor: GridView 열 수를 2개에서 3개로 변경

--- app/src/main/java/com/js/movietrends/presentation/components/MovieListStaggeredGrid.kt
+++ app/src/main/java/com/js/movietrends/presentation/components/MovieListStaggeredGrid.kt
@@ -1,23 +1,28 @@
 package com.js.movietrends.presentation.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.paging.compose.LazyPagingItems
-import coil3.compose.AsyncImage
+import coil3.compose.SubcomposeAsyncImage
+import com.js.movietrends.domain.core.Constants
 import com.js.movietrends.domain.model.Movie
 
 @Composable
 fun MovieListStaggeredGrid(movies: LazyPagingItems<Movie>) {
     LazyVerticalStaggeredGrid(
-        columns = StaggeredGridCells.Fixed(2),
+        columns = StaggeredGridCells.Fixed(3),
         verticalItemSpacing = 4.dp,
         horizontalArrangement = Arrangement.spacedBy(4.dp),
         modifier = Modifier.fillMaxSize(),
@@ -33,14 +38,34 @@ fun MovieListStaggeredGrid(movies: LazyPagingItems<Movie>) {
 
 @Composable
 fun MovieListStaggeredGridItem(movie: Movie) {
-    val aspectRatios = (50..70 step 5).map { it / 100f }
-    val aspectRatio = aspectRatios.random()
-    AsyncImage(
-        model = "https://image.tmdb.org/t/p/w500${movie.posterPath}",
+    val posterUrl =
+        movie.posterPath?.let { "${Constants.POSTER_URL}${Constants.POSTER_XXLARGE}$it" }
+
+    // 영화 id 값으로 포스터 이미지 화면비 결정
+    val movieId = (movie.id ?: 0)
+    val aspectRatioVariant = 5 // 화면비 종류
+    val minAspectRatio = 0.5
+    val maxAspectRatio = 0.7
+    val ratioInterval = (maxAspectRatio - minAspectRatio) / aspectRatioVariant
+    val aspectRatio = (minAspectRatio + movieId % aspectRatioVariant * ratioInterval).toFloat()
+
+    SubcomposeAsyncImage(
+        model = posterUrl,
         contentDescription = "Movie Poster",
         modifier = Modifier
             .fillMaxWidth()
             .aspectRatio(aspectRatio),
-        contentScale = ContentScale.Crop
+        contentScale = ContentScale.Crop,
+        loading = {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.LightGray)
+            )
+        },
+        error = { error ->
+            error.result.throwable.message?.let { Text(it) }
+        },
     )
+
 }
--- domain/src/main/java/com/js/movietrends/domain/core/Constants.kt
+++ domain/src/main/java/com/js/movietrends/domain/core/Constants.kt
@@ -2,6 +2,15 @@ package com.js.movietrends.domain.core
 
 object Constants {
     const val BASIC_URL: String = "https://api.themoviedb.org/3/"
+    const val POSTER_URL: String = "https://image.tmdb.org/t/p/"
+
+    const val POSTER_SMALL = "w92"  // 가장 작은 썸네일 크기
+    const val POSTER_MEDIUM = "w154"
+    const val POSTER_LARGE = "w185"
+    const val POSTER_XLARGE = "w342"
+    const val POSTER_XXLARGE = "w500"  // 기본적으로 많이 사용하는 크기
+    const val POSTER_FULL = "original"  // 원본 크기
+
     const val INTRO_ROUTE: String = "intro_route"
     const val MAIN_ROUTE: String = "main_route"
     const val NOW_PLAYING_ROUTE: String = "now_playing_route"
