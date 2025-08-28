93954576c0fda8b554e67df14ceec53c563fede6
Jisu Kim <kjisubit@gmail.com>
2025-08-28 20:14:36 +0900

    build: kotlin 2.2 적용

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
     alias(libs.plugins.android.library) apply false
 
-    id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
-    id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    id("com.google.devtools.ksp") version "2.2.10-2.0.2" apply false
+    id("com.google.dagger.hilt.android") version "2.57.1" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
