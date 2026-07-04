265a57b241737030a78569595e3525a2065bf475
Jisu Kim <kjisubit@gmail.com>
2026-07-05 00:00:42 +0900

    test: UseCase 대신 Repository를 목킹하도록 테스트 구조 변경

--- app/src/test/java/com/js/movietrends/UseCaseUnitTest.kt
+++ app/src/test/java/com/js/movietrends/UseCaseUnitTest.kt
@@ -5,9 +5,10 @@ import androidx.paging.testing.asSnapshot
 import com.js.movietrends.domain.model.ApiResult
 import com.js.movietrends.domain.model.Movie
 import com.js.movietrends.domain.model.SampleData
+import com.js.movietrends.domain.repository.MovieRepository
 import com.js.movietrends.domain.usecase.GetUpcomingMoviesUseCase
 import com.js.movietrends.domain.usecase.GetWeeklySpotlightedMovieUseCase
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
@@ -17,47 +18,56 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
-import org.mockito.Mockito.mock
 import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 @RunWith(MockitoJUnitRunner::class)
 class UseCaseUnitTest {
 
     @Mock
-    private lateinit var getWeeklySpotlightedMovieUseCase: GetWeeklySpotlightedMovieUseCase
+    private lateinit var movieRepository: MovieRepository
 
-    @Mock
+    private lateinit var getWeeklySpotlightedMovieUseCase: GetWeeklySpotlightedMovieUseCase
     private lateinit var getUpcomingMoviesUseCase: GetUpcomingMoviesUseCase
+
+    private val dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+    private val expectedStartDate = LocalDate.now().minusWeeks(2).format(dateFormatter)
+    private val expectedEndDate = LocalDate.now().minusWeeks(1).format(dateFormatter)
 
     @Before
     fun setup() {
-        whenever(getWeeklySpotlightedMovieUseCase()).thenReturn(
-            flow {
-                emit(
-                    ApiResult.Success(
-                        SampleData.createDummyMovie(
-                            id = 123,
-                            title = "Mock Movie"
-                        )
+        getWeeklySpotlightedMovieUseCase = GetWeeklySpotlightedMovieUseCase(movieRepository)
+        getUpcomingMoviesUseCase = GetUpcomingMoviesUseCase(movieRepository)
+
+        whenever(movieRepository.getDiscoveredMovies(
+            startDate = expectedStartDate,
+            endDate = expectedEndDate,
+            sortBy = "popularity.desc"
+        )).thenReturn(
+            flowOf(
+                ApiResult.Success(
+                    SampleData.createDummyMovie(
+                        id = 123,
+                        title = "Mock Movie"
                     )
                 )
-            }
+            )
         )
 
-        whenever(getUpcomingMoviesUseCase()).thenReturn(
-            flow {
-                emit(
-                    PagingData.from(
-                        List(100) { index ->
-                            SampleData.createDummyMovie(
-                                id = index,
-                                title = "Movie $index"
-                            )
-                        }
-                    )
+        whenever(movieRepository.getUpcomingMovies()).thenReturn(
+            flowOf(
+                PagingData.from(
+                    List(100) { index ->
+                        SampleData.createDummyMovie(
+                            id = index,
+                            title = "Movie $index"
+                        )
+                    }
                 )
-            }
+            )
         )
     }
 
@@ -75,13 +85,20 @@ class UseCaseUnitTest {
         }
 
         // Then: 반환된 영화 정보가 기대한 값과 일치하는지 검증함
-        assertNotNull(movie?.id)
         assertEquals(123, movie?.id)
         assertEquals("Mock Movie", movie?.title)
+
+        // Then: 올바른 파라미터로 Repository가 호출됐는지 검증함
+        @Suppress("UnusedFlow")
+        verify(movieRepository).getDiscoveredMovies(
+            startDate = expectedStartDate,
+            endDate = expectedEndDate,
+            sortBy = "popularity.desc"
+        )
     }
 
     @Test
-    fun useCase_nowPlayingMovieListAvailable() = runTest {
+    fun useCase_upcomingMovieListAvailable() = runTest {
         // Given: 무비 리스트에서 찾고자 하는 인덱스 준비
         val itemIndex = 49
 
