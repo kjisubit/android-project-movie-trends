35fa39fb82d6c0c79f9ca055c2051ce3b1e45368
Jisu Kim <kjisubit@gmail.com>
2026-07-08 20:05:37 +0900

    test: 도메인 모델을 테스트에서 사용하지 않도록 변경

---
+++ app/src/test/java/com/js/movietrends/MovieFixture.kt
@@ -0,0 +1,13 @@
+package com.js.movietrends
+
+import com.js.movietrends.domain.model.Movie
+
+val defaultMovie = Movie(
+    id = 0,
+    title = "Sample Movie",
+    overview = "This is a sample overview",
+    popularity = 1.0,
+    posterPath = null,
+    voteAverage = 5.0,
+    voteCount = 100
+)
